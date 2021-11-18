cb38d57ba3bc74885faabdb4473b8ab09db5a385
Kristian Vassilev <kvasilev@devrix.com>
2021-11-18 16:59:26 +0200

    Working login form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
 
 import './App.css';
 
 const App = () => {
+	if(!localStorage.getItem('username')) return <LoginForm />
+
+
 	return (
 		<ChatEngine
 			height="100vh"
 			projectID="365c730e-33c3-4d2a-a533-36fdd3272ea2"
-			userName="Kristian"
-			userSecret="123"
+			userName={localStorage.getItem('username')}
+			userSecret={localStorage.getItem('password')}
 			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}	
 		/>
 	);
