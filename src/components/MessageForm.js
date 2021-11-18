7cb8c992682d31922a83688d6532103710b0a514
Kristian Vassilev <kvasilev@devrix.com>
2021-11-18 16:02:37 +0200

    Added submit image functionality

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { sendMessage, isTyping } from 'react-chat-engine';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
 
 const MessageForm = (props) => {
 	const [value, setValue] = useState('');
 	const { chatId, creds } = props;
 
 
-	const handleSubmit =(e) => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 
 		const text = value.trim();
@@ -16,10 +17,14 @@ const MessageForm = (props) => {
 		setValue('');
 	}
 
-	const handleChange =(e) => {
+	const handleChange = (e) => {
 		setValue(e.target.value);
 
 		isTyping(props, chatId);
+	}
+
+	const handleUpload = (e) => {
+		sendMessage(creds, chatId, { files: e.target.files, text: '' });
 	}
 
 	return(
@@ -31,6 +36,21 @@ const MessageForm = (props) => {
 				onChange={handleChange}
 				onSubmit={handleSubmit}
 			/>
+			<label htmlFor="upload-button">
+				<span className="image-button">
+					<PictureOutlined className="picture-icon"/>
+				</span>
+			</label>
+			<input
+				type="file"
+				multiple={false}
+				id="upload-button"
+				style={{display: 'none'}}
+				onChange={handleUpload}
+			/>
+			<button type="submit" className="send-button">
+				<SendOutlined className="send-icon"/>
+			</button>
 		</form>
 	);
 }
