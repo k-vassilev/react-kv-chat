62d1d1e34a7bb3dbecea36d3220744227aac3a17
Kristian Vassilev <kvasilev@devrix.com>
2021-11-18 15:50:09 +0200

    Added working message form and working one-sided chat

--- src/components/MessageForm.js
+++ src/components/MessageForm.js
@@ -1,8 +1,37 @@
-const MessageForm = () => {
+import { useState } from 'react';
+import { sendMessage, isTyping } from 'react-chat-engine';
+
+const MessageForm = (props) => {
+	const [value, setValue] = useState('');
+	const { chatId, creds } = props;
+
+
+	const handleSubmit =(e) => {
+		e.preventDefault();
+
+		const text = value.trim();
+
+		if(text.length > 0) sendMessage(creds, chatId, { text });
+
+		setValue('');
+	}
+
+	const handleChange =(e) => {
+		setValue(e.target.value);
+
+		isTyping(props, chatId);
+	}
+
 	return(
-		<div>
-			MessageForm
-		</div>
+		<form className="message-form" onSubmit={handleSubmit}>
+			<input
+				className="message-input"
+				placeholder="Send a message ..."
+				value={value}
+				onChange={handleChange}
+				onSubmit={handleSubmit}
+			/>
+		</form>
 	);
 }
 
