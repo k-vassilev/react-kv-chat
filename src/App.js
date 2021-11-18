ad7982a90fcaa7804feeb98552e4da2c1e6a4774
Kristian Vassilev <kvasilev@devrix.com>
2021-11-18 15:21:31 +0200

    Added logic for Chatfeed and MyMessage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
+import ChatFeed from './components/ChatFeed';
+
 import './App.css';
 
 const App = () => {
@@ -8,7 +10,8 @@ const App = () => {
 			height="100vh"
 			projectID="365c730e-33c3-4d2a-a533-36fdd3272ea2"
 			userName="Kristian"
-			userSecret="123"		
+			userSecret="123"
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}	
 		/>
 	);
 }
